808583597df7de42e32c0779a9153ed80039eb22
luca1705 <51916830+TheLucanus@users.noreply.github.com>
2023-02-13 17:42:19 +0100

    Implementation of NMF in PyTorch
    First Pass at implementing NMF in PyTorch, As well as a seperate data.py file, to load and normalize/inverse normalize data

---
+++ data.py
@@ -0,0 +1,23 @@
+import scipy.io
+import numpy as np
+import matplotlib.pyplot as plt
+
+#load data from .MAT file
+mat = scipy.io.loadmat('data/nmrdata.mat')
+
+#Get X and Labels. Probably different for the other dataset, but i didn't check :)
+X = mat.get('nmrdata')[0][0][0]
+labels = mat.get('nmrdata')[0][0][0]
+
+#Store mean and std for inversing the normalization
+mu_Y = np.mean(X)
+std_U = np.std(X)
+
+#functions for normalizing, and inversing the normalization of data
+def normalize_data(target):
+    return (target - np.mean(target))/np.std(target)
+
+def inv_normalize_data(target, mean, std):
+    return target * std + mean
+
+X = normalize_data(X)
