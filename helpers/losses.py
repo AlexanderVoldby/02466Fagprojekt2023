5c0a8e67523955c038beaa7e6c2927a5ea861aa9
luca1705 <51916830+TheLucanus@users.noreply.github.com>
2023-03-13 14:16:41 +0100

    mergin error fixed

--- helpers/losses.py
+++ helpers/losses.py
@@ -9,11 +9,3 @@ class frobeniusLoss(torch.nn.Module):
     
     def forward(self, input):
         return self.loss(self.X - input, ord='fro')
-
-class frobeniusLoss(torch.nn.Module):
-    def __init__(self):
-        super(frobeniusLoss, self).__init__()
-        self.loss = torch.linalg.matrix_norm
-    
-    def forward(self, input):
-        return self.loss(input)
