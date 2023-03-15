014d0398fdb733a0bd45149e1ee4c59be7fe5cff
AlexanderVoldby <alexander@murmann.dk>
2023-03-15 13:45:57 +0100

    Created artificial dataset for plotting and defined training method for NMF

--- torchNMFtopy.py
+++ torchNMFtopy.py
@@ -1,6 +1,4 @@
 import torch
-import numpy as np
-import pandas as pd
 
 from helpers.data import X
 from helpers.callbacks import earlyStop
@@ -9,13 +7,14 @@ from helpers.losses import frobeniusLoss
 
 class torchNMF(torch.nn.Module):
     def __init__(self, X, rank):
-        super(torchNMF, self).__init__()
+        super().__init__()
 
         # Shape of Matrix for reproduction
         n_row, n_col = X.shape
         self.X = torch.tensor(X)
-
         self.softplus = torch.nn.Softplus()
+        self.optim = torch.optim.Adam(self.parameters(), lr=0.3)
+        self.lossfn = frobeniusLoss(self.X)
 
         # Initialization of Tensors/Matrices a and b with size NxR and RxM
         self.A = torch.nn.Parameter(torch.rand(n_row, rank, requires_grad=True))
@@ -23,62 +22,37 @@ class torchNMF(torch.nn.Module):
 
     def forward(self):
         # Implementation of NMF - F(A, B) = ||X - AB||^2
-        self.AB = torch.matmul(self.softplus(self.A),
-                               self.softplus(self.B))
-        return self.AB
+        AB = torch.matmul(self.softplus(self.A), self.softplus(self.B))
+        return AB
 
+    def run(self, verbose=False):
+        es = earlyStop(patience=5, offset=-0.1)
+        running_loss = []
+        while not es.trigger():
+            # zero optimizer gradient
+            self.optim.zero_grad()
 
-torch.manual_seed(0)
+            # forward
+            output = self.forward()
 
-nmf = torchNMF(X, 3)
+            # backward
 
-# optimizer for modifying learning rate, ADAM chosen because of https://machinelearningmastery.com/adam-optimization-algorithm-for-deep-learning/
-optimizer = torch.optim.Adam(nmf.parameters(), lr=0.3)
-lossFN = frobeniusLoss(X)
-# early stopping
-es = earlyStop(patience=5, offset=-0.1)
+            loss = self.lossfn.forward(output)
+            loss.backward()
 
-running_loss = []
+            # Update A and B
+            self.optim.step()
 
-while (not es.trigger()):
-    # zero optimizer gradient
-    optimizer.zero_grad()
+            running_loss.append(loss.item())
+            es.count(loss.item())
 
-    # forward
-    output = nmf()
+            # print loss
+            if verbose:
+                print(f"epoch: {len(running_loss)}, Loss: {loss.item()}", end='\r')
 
-    # backward
+        A, B = list(self.parameters())
 
-    loss = lossFN.forward(output)
-    loss.backward()
+        A = self.softplus(A)
+        B = self.softplus(B)
 
-    # Update A and B
-    optimizer.step()
-
-    running_loss.append(loss.item())
-    es.count(loss.item())
-
-    # print loss
-    print(f"epoch: {len(running_loss)}, Loss: {loss.item()}", end='\r')
-
-A, B = list(nmf.parameters())
-
-A = nmf.softplus(A)
-B = nmf.softplus(B)
-
-A = A.detach().numpy()
-B = B.detach().numpy()
-
-# rec = np.dot(A, B)
-
-# rec = rec.T
-
-# rec_frame = pd.DataFrame(rec)
-# rec_frame.columns = rec_frame.columns.astype(str)
-
-# rec_frame.to_parquet("recons_x_nmf.parquet",
-                     # engine='fastparquet')
-# import matplotlib.pyplot as plt
-
-# plt.plot(rec[0])
- #plt.show()
+        return A, B
