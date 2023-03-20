a22ec66a6cf4d3006d06369f7e922f30f9fc92df
Lucas Sylvester <lucassylvester02@gmail.com>
2023-03-20 13:37:38 +0100

    First try at implementing vol min

--- helpers/losses.py
+++ helpers/losses.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 #Defining frobenius Loss
 class frobeniusLoss(torch.nn.Module):
@@ -9,3 +10,12 @@ class frobeniusLoss(torch.nn.Module):
     
     def forward(self, input):
         return self.loss(self.X - input, ord='fro')
+    
+class VolLoss(torch.nn.Module):
+    def __init__(self, x: torch.tensor):
+        super(VolLoss, self).__init__()
+        self.loss = torch.linalg.det
+        self.X = x
+
+    def forward(self, w, h, x):
+        return self.loss((w.T@w))+torch.linalg.matrix_norm(self.X - x, ord='fro')
