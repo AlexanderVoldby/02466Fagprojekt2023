d4dc94e0bd157c2367d6826752af39b9cfa3172c
AlexanderVoldby <alexander@murmann.dk>
2023-03-28 11:18:56 +0200

    Renmaed files and deleted notebooks

--- helpers/losses.py
+++ helpers/losses.py
@@ -18,6 +18,8 @@ class VolLoss(torch.nn.Module):
         self.X = x
 
     def forward(self, w, h, x):
+        #TODO: Square frobenius norm and add regularization
+        # We might have to change to constraining on H since it should denote the source.
         return self.loss((w.T@w))+torch.linalg.matrix_norm(self.X - x, ord='fro')
 
 # Sparseness measure of the H-matrix
