Fixed AA and continued work on alcohol dataset. Implemented function for calculating explained variance

ShiftNMF.py
@@ -42,6 +42,7 @@ class ShiftNMF(torch.nn.Module):
         # Broadcast Wf and H together
         V = torch.einsum('NdM,dM->NM', Wf, Hf)
         return V
+
     def fit(self, verbose=False):
         es = earlyStop(patience=5, offset=-0.01)
         running_loss = []
@@ -79,22 +80,22 @@ class ShiftNMF(torch.nn.Module):
 
 
 if __name__ == "__main__":
-    nmf = ShiftNMF(X_clean, 2, shift_constraint=5)
+    nmf = ShiftNMF(X_clean, 4, shift_constraint=5)
     nmf.to(device)
     W, H, tau = nmf.fit(verbose=True)
 
-    # plt.figure()
-    # for signal in H:
-    #     plt.plot(signal)
-    # plt.title("H - the latent variables")
-    # plt.show()
+    plt.figure()
+    for signal in H:
+        plt.plot(signal)
+    plt.title("H - the latent variables")
+    plt.show()
 
 
-    # plt.figure()
-    # plt.imshow(W)
-    # plt.colorbar()
-    # plt.title("W - The mixings")
-    # plt.show()
+    plt.figure()
+    plt.imshow(W)
+    plt.colorbar()
+    plt.title("W - The mixings")
+    plt.show()
 
     plt.figure()
     plt.imshow(tau)

ShiftNMF_MVR.py
@@ -28,19 +28,19 @@ class ShiftNMF(torch.nn.Module):
         self.W = torch.nn.Parameter(torch.rand(self.N, rank, requires_grad=True))
         self.H = torch.nn.Parameter(torch.rand(rank, self.M, requires_grad=True))
         # TODO: Constrain tau by using tanh and multiplying with a max/min value
-        self.tau = torch.nn.Parameter(torch.tanh(torch.rand(self.N, self.rank) * 2000 - 1000), requires_grad=True)
+        self.tau = torch.nn.Parameter(torch.rand(self.N, self.rank), requires_grad=True)
         self.optim = torch.optim.Adam(self.parameters(), lr=0.1)
 
     def forward(self):
         Hf = torch.fft.fft(self.softplus(self.H)) # DFT on H
         f = torch.arange(0, self.M) / self.M # The frequencies
         omega = torch.exp(-1j*2*torch.pi*torch.einsum('ab,c->abc', self.tau, f)) # The shift operator
-        Wf = torch.einsum('ab,abc->abc', self.softmax(self.W, dim=1), omega) # Multiply shift operator into W
+        Wf = torch.einsum('ab,abc->abc', self.softmax(self.W, dim=0), omega) # Multiply shift operator into W
         V = torch.einsum('abc,bc->ac', Wf, Hf) # Multiply Wf and Hf
         return V
 
     def fit(self, verbose=False):
-        es = earlyStop(patience=5, offset=-0.1)
+        es = earlyStop(patience=5, offset=-0.001)
         running_loss = []
         while not es.trigger():
             # zero optimizer gradient
@@ -50,7 +50,7 @@ class ShiftNMF(torch.nn.Module):
             output = self.forward()
 
             # backward
-            loss = self.lossfn(output, self.softplus(self.H))
+            loss = self.lossfn(output, self.softmax(self.W, dim=0))
             loss.backward()
 
             # Update W, H and tau
@@ -65,8 +65,8 @@ class ShiftNMF(torch.nn.Module):
 
         W, H, tau = list(self.parameters())
 
-        W = self.softmax(W, dim=1).detach().numpy()
-        H = self.softplus(H).detach().numpy()
+        W = self.softmax(self.W, dim=0).detach().numpy()
+        H = self.softplus(self.H).detach().numpy()
         tau = tau.detach().numpy()
 
         return W, H, tau
@@ -74,7 +74,6 @@ class ShiftNMF(torch.nn.Module):
 
 if __name__ == "__main__":
     nmf = ShiftNMF(X, 4)
-    nmf.to(device)
     W, H, tau = nmf.fit(verbose=True)
 
     plt.figure()

ShiftNMF_half_frequencies.py
@@ -28,7 +28,7 @@ class ShiftNMF(torch.nn.Module):
         self.H = torch.nn.Parameter(torch.rand(rank, self.M, requires_grad=True))
         # TODO: Constrain tau by using tanh and multiplying with a max/min value
         # self.tau = torch.nn.Parameter(torch.tanh(torch.rand(self.N, self.rank) * 2000 - 1000), requires_grad=True)
-        self.tau = torch.nn.Parameter(torch.rand(self.N, self.rank)*2*self.shift_constraint-self.shift_constraint, requires_grad=True)
+        self.tau = torch.nn.Parameter(torch.rand(self.N, self.rank), requires_grad=True)
         self.optim = torch.optim.Adam(self.parameters(), lr=0.2)
 
     def forward(self):
@@ -80,8 +80,7 @@ class ShiftNMF(torch.nn.Module):
 
         W = self.softplus(W).detach().numpy()
         H = self.softplus(H).detach().numpy()
-        tau = torch.tanh(tau.detach()).numpy() * self.shift_constraint
-        # tau = tau.detach().numpy()
+        tau = tau.detach().numpy()
 
         return W, H, tau
 

helpers/losses.py
@@ -8,7 +8,7 @@ class frobeniusLoss(torch.nn.Module):
         self.X = x
     
     def forward(self, input):
-        return torch.linalg.matrix_norm(self.X - input, ord='fro')**2
+        return torch.linalg.matrix_norm(self.X - input, ord='fro')
 
 
 class ShiftNMFLoss(torch.nn.Module):
@@ -67,9 +67,9 @@ class MVR_ShiftNMF_Loss(torch.nn.Module):
         self.lamb = lamb
         self.eps = 1e-6
 
-    def forward(self, inp, H): # Loss function must take the reconstruction and H.
-        loss = 1 / (2 * self.M) * torch.linalg.matrix_norm(self.Xf - inp, ord='fro')
-        vol_H = torch.log(torch.det(torch.matmul(H, H.T) + self.eps))
-        reg = self.lamb/2 * vol_H
+    def forward(self, inp, W): # Loss function must take the reconstruction and H.
+        loss = 1 / (2 * self.M) * torch.linalg.matrix_norm(self.Xf - inp, ord='fro')**2
+        vol_W = torch.det(torch.matmul(W.T, W) + self.eps)
+        reg = self.lamb/2 * vol_W
         print(f"Loss: {loss}, Regularization: {reg}")
         return loss + reg

shifted_dataset.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import torch
 from ShiftNMF import ShiftNMF
 np.random.seed(42069)
 
@@ -10,17 +9,6 @@ Fs = 1000  # The sampling frequency we use for the simulation
 t0 = 10     # The half-time interval we look at
 t = np.arange(-t0, t0, 1/Fs)  # the time samples
 f = np.arange(-Fs/2, Fs/2, Fs/len(t))  # the corresponding frequency samples
-
-def ft(samples, Fs, t0):
-    """Approximate the Fourier Transform of a time-limited
-    signal by means of the discrete Fourier Transform.
-
-    samples: signal values sampled at the positions t0 + n/Fs
-    Fs: Sampling frequency of the signal
-    t0: starting time of the sampling of the signal. We assume it is zero.
-    """
-    f = np.linspace(-Fs / 2, Fs / 2, len(samples), endpoint=False)
-    return np.fft.fftshift(np.fft.fft(samples) / Fs * np.exp(-2j * np.pi * f * t0))
 
 def gauss(mu, s, time):
     return 1/(s*np.sqrt(2*np.pi))*np.exp(-1/2*((time-mu)/s)**2)
@@ -53,7 +41,7 @@ def shift_dataset(W, H, tau):
 # Random mixings:
 W = np.random.rand(N, d)
 # Random gaussian shifts
-tau = np.random.randn(N, d)*50
+tau = np.random.randint(0, 1000, size=(N, d))
 # Purely positive underlying signals. I define them as 3 gaussian peaks with random mean and std.
 mean = [40, 300, 700]
 std = [10, 20, 7]
@@ -75,7 +63,7 @@ plt.show()
 
 # Try to find real components with shiftNMF:
 shiftnmf = ShiftNMF(X, 3)
-W_, H_, tau_ = shiftnmf.fit(verbose=True)
+W_, H_, tau_ = shiftnmf.fit()
 print(W_)
 print(W)
 # Plot the signals found by shiftNMF

torchAA.py
@@ -28,17 +28,15 @@ class torchAA(torch.nn.Module):
         # Implementation of AA - F(C, S) = ||X - XCS||^2
 
         # first matrix Multiplication with softmax
-        self.CX = torch.matmul(self.C().double(), self.X.double())
+        CX = torch.matmul(self.C().double(), self.X.double())
 
         # Second matrix multiplication with softmax
-        self.SCX = torch.matmul(self.S().double(),self.CX.double())
+        SCX = torch.matmul(self.S().double(), CX.double())
 
-        x = self.X - self.SCX
-
-        return x
+        return SCX
 
     def fit(self, verbose=False):
-        optimizer = torch.optim.Adam(self.parameters(), lr=0.4)
+        optimizer = torch.optim.Adam(self.parameters(), lr=0.3)
 
         # early stopping
         es = earlyStop(patience=5, offset=-0.001)
@@ -66,7 +64,7 @@ class torchAA(torch.nn.Module):
             es.count(loss.item())
 
             # print loss
-            if verbose:
+            if verbose and len(running_loss) % 50 == 0:
                 print(f"epoch: {len(running_loss)}, Loss: {loss.item()}", end='\r')
 
         C, S = list(self.parameters())
@@ -89,20 +87,20 @@ if __name__ == "__main__":
 
     #Get X and Labels. Probably different for the other dataset, but i didn't check :)
     X = mat.get('xData')
-
-    
     
     AA = torchAA(X, 3)
-    C,S = AA.fit(verbose=True)
+    C, S = AA.fit(verbose=True)
     CX = np.matmul(C, X)
-    SCX = np.matmul(S,CX)
-    print()
-    plt.plot(CX[0])
-    plt.plot(CX[1])
-    plt.plot(CX[2])
+    SCX = np.matmul(S, CX)
+    plt.figure()
+    for vec in CX:
+        plt.plot(vec)
+    plt.title("Archetypes")
     plt.show()
-    plt.plot(X[1])
-    plt.plot(SCX[1])
+    plt.figure()
+    plt.plot(X[1], label="First signal of X")
+    plt.plot(SCX[1], label="Reconstructed signal with AA")
+    plt.legend()
     plt.show()
 
     plt.plot(X[2])

torchNMF.py
@@ -5,7 +5,7 @@ from helpers.callbacks import earlyStop
 from helpers.losses import frobeniusLoss
 
 
-class torchNMF(torch.nn.Module):
+class NMF(torch.nn.Module):
     def __init__(self, X, rank):
         super().__init__()
 
@@ -18,10 +18,9 @@ class torchNMF(torch.nn.Module):
         self.W = torch.nn.Parameter(torch.rand(n_row, rank, requires_grad=True))
         self.H = torch.nn.Parameter(torch.rand(rank, n_col, requires_grad=True))
 
-        self.optim = torch.optim.Adam(self.parameters(), lr=0.03)
+        self.optim = torch.optim.Adam(self.parameters(), lr=0.3)
 
     def forward(self):
-        # Implementation of NMF - F(A, B) = ||X - AB||^2
         WH = torch.matmul(self.softplus(self.W), self.softplus(self.H))
         return WH
 
@@ -49,9 +48,6 @@ class torchNMF(torch.nn.Module):
             # print loss
             if verbose and len(running_loss) % 50 == 0:
                 print(f"epoch: {len(running_loss)}, Loss: {loss.item()}")
-
-        if verbose:
-            print(f"Final loss: {running_loss[-1]}")
 
         W, H = list(self.parameters())
 