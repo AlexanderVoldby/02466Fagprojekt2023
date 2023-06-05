9e3b1705c5013c7ca35501541a653e295d433812
AlexanderVoldby <alexander@murmann.dk>
2023-06-05 12:37:34 +0200

    Deleted all other stoppers than changestopper, they dont work so stop using them!

--- helpers/callbacks.py
+++ helpers/callbacks.py
@@ -32,41 +32,6 @@ class Stopper:
     def trigger(self):
         pass
 
-
-class EarlyStop(Stopper):
-    def __init__(self, patience = 5, offset = 0) -> None:
-        self.patience = patience
-        
-        self.counter = 0
-        self.lowest = np.Inf
-        
-        self.offset = offset
-        
-    def track_loss(self, loss_val):
-        if loss_val < self.lowest + self.offset:
-            self.lowest = loss_val
-            self.counter = 0
-        else:
-            self.counter += 1
-            
-    def trigger(self):
-        return self.counter > self.patience
-
-
-class RelativeStopper(Stopper):
-    def __init__(self, data, alpha=1e-6):
-        self.norm = torch.linalg.matrix_norm(data, ord="fro").item()**2
-        self.alpha = alpha
-        self.loss = 1e9
-
-    def track_loss(self, loss):
-        self.loss = loss
-
-    def trigger(self):
-        return self.loss/self.norm < self.alpha
-
-
-# 
 class ChangeStopper(Stopper):
     def __init__(self, alpha=1e-8, patience=5):
         self.alpha = alpha
