dd4c12485e9127b280a1c0f7e64bf4737adca4c8
luca1705 <lucas.morch@gmail.com>
2023-06-07 13:56:52 +0200

    Updated Learning rate and scheduler for AA such that it converges better

--- torchAA.py
+++ torchAA.py
@@ -4,7 +4,7 @@ from helpers.callbacks import ChangeStopper
 from helpers.losses import frobeniusLoss
 from helpers.initializers import FurthestSum
 
-
+import scipy.io
 
 class torchAA(torch.nn.Module):
     def __init__(self, X, rank, alpha=1e-9):
@@ -19,11 +19,23 @@ class torchAA(torch.nn.Module):
         
         self.C = torch.nn.Parameter(torch.randn(rank, N, requires_grad=True, dtype=torch.double)*3)
         self.S = torch.nn.Parameter(torch.randn(N, rank, requires_grad=True, dtype=torch.double)*3)
-        self.optimizer = Adam(self.parameters(), lr=0.5)
+        
+        # mat = scipy.io.loadmat('helpers/PCHA/C.mat')
+        # self.C = mat.get('c')
+        # self.C = torch.tensor(self.C, requires_grad=True, dtype=torch.double)
+        # self.C = torch.nn.Parameter(self.C.T)
+        
+        # mat = scipy.io.loadmat('helpers/PCHA/S.mat')
+        # self.S = mat.get('s')
+        # self.S = torch.tensor(self.S, requires_grad=True, dtype=torch.double)
+        # self.S = torch.nn.Parameter(self.S.T)
+        
+        
+        self.optimizer = Adam(self.parameters(), lr=0.2)
         self.stopper = ChangeStopper(alpha=alpha)
-        self.scheduler = lr_scheduler.ReduceLROnPlateau(self.optimizer, mode='min', factor=0.1, patience=5)
+        self.scheduler = lr_scheduler.ReduceLROnPlateau(self.optimizer, mode='min', factor=0.9, patience=5)
 
-
+ 
     def forward(self):
 
         # first matrix Multiplication with softmax
@@ -61,7 +73,7 @@ class torchAA(torch.nn.Module):
 
             # print loss
             if verbose:
-                print(f"epoch: {len(running_loss)}, Loss: {loss.item()}", end='\r')
+                print(f"epoch: {len(running_loss)}, Loss: {1-loss.item()}", end='\r')
 
         C = self.softmax(self.C)
         S = self.softmax(self.S)
