6725e1180a2634824d0295b7483637284c4ea9bd
luca1705 <lucas.morch@gmail.com>
2023-06-07 16:50:51 +0200

    Adjusted Learning rate

--- torchNMF.py
+++ torchNMF.py
@@ -73,8 +73,8 @@ class MVR_NMF(torch.nn.Module):
         self.W = torch.nn.Parameter(torch.rand(n_row, rank, requires_grad=True))
         self.H = torch.nn.Parameter(torch.rand(rank, n_col, requires_grad=True))
 
-        self.optim = Adam(self.parameters(), lr=0.3)
-        self.scheduler = lr_scheduler.ReduceLROnPlateau(self.optim, mode='min', factor=0.1, patience=5)
+        self.optim = Adam(self.parameters(), lr=0.5)
+        self.scheduler = lr_scheduler.ReduceLROnPlateau(self.optim, mode='min', factor=0.9, patience=5)
 
     def forward(self):
         WH = torch.matmul(self.softmax(self.W), self.softplus(self.H))
