b1ba98a5e417db0e980715312e00589036639802
AlexanderVoldby <alexander@murmann.dk>
2023-06-11 11:21:34 +0200

    regularization test

--- Regularization_test.py
+++ Regularization_test.py
@@ -11,7 +11,7 @@ def plot_components(matrix, title):
     plt.suptitle(title)
     plt.show()
 
-def regularization_experiment(lower, upper, X, reg_mode, n_train=2, components=3):
+def regularization_experiment(lower, upper, X, reg_mode, n_train=1, components=3):
     regs = np.logspace(lower, upper, num=abs(lower-upper), endpoint=False)
     explained_vars = []
     hs = []
@@ -33,15 +33,15 @@ targets = mat.get('yData')
 target_labels = mat.get('yLabels')
 axis = mat.get("Axis")
 
-regs1, exp_var_1, hs_1, ws_1 = regularization_experiment(-34, -24, X, 1)
-regs2, exp_var_2, hs_2, vs_2 = regularization_experiment(-34, -24, X, 2)
+regs1, exp_var_1, hs_1, ws_1 = regularization_experiment(-40, -30, X, 1)
+regs2, exp_var_2, hs_2, vs_2 = regularization_experiment(-40, -30, X, 2)
 
-# fig, ax = plt.subplots()
-# ax.plot(regs1, exp_var_1, marker="o", linestyle="--")
-# ax.set_xlabel("Regularization Strength")
-# ax.set_ylabel("Explained variance using L1 normalization")
-# ax.set_xscale("log")
-# plt.show()
+fig, ax = plt.subplots()
+ax.plot(regs1, exp_var_1, marker="o", linestyle="--")
+ax.set_xlabel("Regularization Strength")
+ax.set_ylabel("Explained variance using L1 normalization")
+ax.set_xscale("log")
+plt.show()
 
 fig, ax = plt.subplots()
 ax.plot(regs2, exp_var_2, marker="o", linestyle="--")
@@ -50,8 +50,8 @@ ax.set_ylabel("Explained variance using L2 normalization")
 ax.set_xscale("log")
 plt.show()
 
-# for h, reg in zip(hs_1, regs1):
-#     plot_components(h, f"Latent components with regularization {reg}")
+for h, reg in zip(hs_1, regs1):
+    plot_components(h, f"Latent components with regularization {reg}")
 
 for h, reg in zip(hs_2, regs2):
     plot_components(h, f"Latent components with regularization {reg}")
