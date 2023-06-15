9b7708825c97353a2b0873aade8f8af96b8aa5f4
AlexanderVoldby <alexander@murmann.dk>
2023-06-15 13:34:38 +0200

    Prepared regularization test for the hpc

--- Regularization_test.py
+++ Regularization_test.py
@@ -1,11 +1,16 @@
 import scipy
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 from helpers.callbacks import explained_variance, plot_data, train_n_times
 import pickle
 from torchNMF import MVR_NMF
 
-
+lower = -42
+upper = -30
+regs = np.logspace(lower, upper, num=2*abs(lower-upper), endpoint=False)
+num_regs = len(regs)
+x = sys.argv[1]
 
 def plot_components(matrix, title):
     fig, ax = plt.subplots(1, matrix.shape[0])
@@ -14,20 +19,13 @@ def plot_components(matrix, title):
     plt.suptitle(title)
     plt.show()
 
-def regularization_experiment(lower, upper, X, reg_mode, n_train=3, components=3):
-    regs = np.logspace(lower, upper, num=2*abs(lower-upper), endpoint=False)
-    explained_vars = []
-    hs = []
-    ws = []
-    for reg in regs:
-        print(f"Current regularization strength: {reg}")
-        (W, H), loss = train_n_times(n_train, MVR_NMF, X, components, regularization=reg, normalization=reg_mode)
-        recon = np.matmul(W, H)
-        explained_vars.append(explained_variance(X, recon))
-        hs.append(H)
-        ws.append(W)
+def regularization_experiment(reg, X, reg_mode, n_train=5, components=3):
+    print(f"Current regularization strength: {reg}")
+    (W, H), loss = train_n_times(n_train, MVR_NMF, X, components, regularization=reg, normalization=reg_mode)
+    recon = np.matmul(W, H)
+    explained_var = explained_variance(X, recon)
 
-    return regs, explained_vars, hs, ws
+    return reg, explained_var, W, H
 
 
 mat = scipy.io.loadmat('helpers/data/NMR_mix_DoE.mat')
@@ -36,14 +34,14 @@ targets = mat.get('yData')
 target_labels = mat.get('yLabels')
 axis = mat.get("Axis")
 
-regs1, exp_var_1, hs_1, ws_1 = regularization_experiment(-42, -30, X, 1)
-regs2, exp_var_2, hs_2, ws_2 = regularization_experiment(-42, -30, X, 2)
+reg1, exp_var_1, w1, h1 = regularization_experiment(regs[x], X, 1)
+reg2, exp_var_2, w2, h2 = regularization_experiment(regs[x], X, 2)
 
 # Save regs1 and exp_var_1 to "regularization_L1" file
-with open("Results/Regularization/regularization_L1", "wb") as f:
-    pickle.dump((regs1, exp_var_1, hs_1, ws_1), f)
+with open(f"Results/Regularization/L1_reg_{reg1}", "wb") as f:
+    pickle.dump((reg1, exp_var_1, w1, h1), f)
 
 # Save regs2 and exp_var_2 to "regularization_L2" file
-with open("Results/Regularization/regularization_L2", "wb") as fb:
-    pickle.dump((regs2, exp_var_2, hs_2, ws_2), fb)
+with open(f"Results/Regularization/L2_reg_{reg2}", "wb") as fb:
+    pickle.dump((reg2, exp_var_2, w2, h2), fb)
 
